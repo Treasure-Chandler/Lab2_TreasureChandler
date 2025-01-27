5ff54e7483720d82ebb3afc8bc49339ec544835e
Treasure-Chandler <tchandler1211@gmail.com>
2025-01-27 10:52:01 -0500

    More progress on the party class

--- Lab2_TreasureChandler/src/Party.java
+++ Lab2_TreasureChandler/src/Party.java
@@ -21,8 +21,58 @@ public class Party {
      * @param hero      Hero to be added to the party
      */
     public void addHero(int index, Hero hero) {
-        for (index = 0; index < heroes.length; index++) {
-            
+        // Check for handling invalid indices
+        if (index < 0 || index >= heroes.length) {
+            throw new IndexOutOfBoundsException("Index " + index + " is out of bounds for " + 
+                                                "the size of this party!");
         }
+
+        // Adding hero magic
+        heroes[index] = hero;
     } // End of addHero()
+
+    /**
+     * Remove and return the Hero from the given index
+     * 
+     * @param index     Index
+     * @return          Hero to be removed from the array
+     */
+    public Hero removeHero(int index) {
+        // Check for handling an out of bounds index
+        if (index < 0 || index >= heroes.length) {
+            throw new IndexOutOfBoundsException("Index " + index + " is out of bounds for " + 
+                                                "the size of this party!");
+        }
+
+        // Check for handling if there is no hero at the index
+        if (heroes[index] == null) {
+            throw new IllegalArgumentException("There is no hero that exists at index " +
+                                                index + "!");
+        }
+
+        // Variable declaration
+        Hero removedHero = heroes[index];
+
+        // Remove hero at the given index
+        heroes[index] = null;
+
+        return removedHero;
+    } // End of removeHero()
+
+    /**
+     * Return the hero of the desired index
+     * 
+     * @param index     Index
+     * @return          Hero to be fetched
+     */
+    public Hero getHero(int index) {
+        // Variable declaration
+        Hero fetchedHero = heroes[index];
+
+        return fetchedHero;
+    } // End of getHero()
+
+    public void gainExperience() {
+
+    } // End of gainExperience()
 } // End of Party
